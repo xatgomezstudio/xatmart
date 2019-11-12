35cc8323520549ea3d2c1dd9dfa2d97fef2b59e8
Catherine Gomez <catherinegomezstudio@gmail.com>
2019-11-12 01:28:34 -0500

    prompt quantity and id working

--- xatmartCustomer.js
+++ xatmartCustomer.js
@@ -22,48 +22,50 @@ function welcome() {
         .prompt({
             name: "Welcome",
             type: "list",
-            message: "Quick and incognito, what do you want?",
-            choices: ["STUFF", "NOTHIN"]
+            message: "Quick, ya wanna buy sumthin?",
+            choices: ["Yea, what'd ya got?", "Nah, leave me alone!"]
         }).then(function (answer) {
-            if (answer.Welcome === "STUFF") {
+            if (answer.Welcome === "Yea, what'd ya got?") {
                 inventory();
-            } else (answer.Welcome === "NOTHIN")
-            // boiBye();
+            } else (answer.Welcome === "Nah, leave me alone!")
+            boiBye();
         })
 };
 
 //products listed
 function inventory() {
-    console.log("This is my haul today...");
-    connection.query("SELECT * FROM products", function (err, results) {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
-        // Log all results of the SELECT statement
-        console.log(divider + results + divider);
+        console.log("This is my haul today...")
+        for (var i = 0; i < res.length; i++) {
+            console.log(divider);
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+        };
+        customerChooses()
     });
-    // customerChooses();
-}
+};
 
-// function customerChooses() {
-//     inquirer
-//         .prompt([{
-//             name: "inputId",
-//             type: "input",
-//             message: "Wha'ya wanna buy, what's the id number?"
-//         }, {
-//             name: "inputQuantity",
-//             type: "input",
-//             message: "How many do you want?"
-//         }]).then(function (answer) {
-//             console.log("So you want " + answers.Quantity + " # " + answers.inputId);
-//             connection.query("SELECT * FROM products WHERE item_id=?", [answer.inputId], function (err, results) {
-//                 if (err) throw err;
-//                 console.log(results);
-//             });
-//         });
-//     boiBye();
-// };
+function customerChooses() {
+    inquirer
+        .prompt([{
+            name: "inputId",
+            type: "input",
+            message: "Wha'ya wanna buy, what's the id number?"
+        }, {
+            name: "inputQuantity",
+            type: "input",
+            message: "How many ya want?"
+        }]).then(function (answer) {
+            console.log("So you want " + answers.Quantity + " # " + answers.inputId);
+            connection.query("SELECT * FROM products WHERE item_id=?", [answer.inputId], function (err, results) {
+                if (err) throw err;
+                console.log(results);
+            });
+        });
+};
+// boiBye();
 
-// //ends connection
+//ends connection
 function boiBye() {
     console.log("Shit it's the cops!")
     connection.end();
